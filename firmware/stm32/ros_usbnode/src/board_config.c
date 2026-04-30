5556c2aafb5223cf6c8c091d3ce558e1c013c32c
Pepeuch <pepeuch@gmail.com>
2026-04-30 14:41:13 +0200

    refactor(firmware): introduce HAL + BSP (GPIO, UART, time) with multi-board support (F103/F401)

--- firmware/stm32/ros_usbnode/src/board_config.c
+++ firmware/stm32/ros_usbnode/src/board_config.c
@@ -1,7 +1,5 @@
-#include "stm32f_board_hal.h"
 #include "board_config.h"
-#include "board.h"
 
-const hal_pin_t BOARD_STATUS_LED = {LED_GPIO_PORT, LED_PIN};
-const hal_pin_t BOARD_RAIN_SENSOR = {RAIN_SENSOR_PORT, RAIN_SENSOR_PIN};
-const hal_pin_t BOARD_TF4_SWITCH = {TF4_GPIO_PORT, TF4_PIN};
+/*
+ * Board pin definitions are provided by boards/<board>/board_pins.c.
+ */
---
+++ firmware/stm32/ros_usbnode/src/boards/yardforce500b/board_pins.c
@@ -0,0 +1,11 @@
+#if BOARD_YARDFORCE500_VARIANT_B
+
+#include "stm32f_board_hal.h"
+#include "board_config.h"
+#include "board.h"
+
+const hal_pin_t BOARD_STATUS_LED = {LED_GPIO_PORT, LED_PIN};
+const hal_pin_t BOARD_RAIN_SENSOR = {RAIN_SENSOR_PORT, RAIN_SENSOR_PIN};
+const hal_pin_t BOARD_TF4_SWITCH = {TF4_GPIO_PORT, TF4_PIN};
+
+#endif 
